8f1a3f90582b3abb440d0c676db2310c162ab23e
Maria Elizarova <yobs.mary@gmail.com>
2024-07-16 18:27:01 +0400

    init state

---
+++ crawler.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer-core')
+
+const URL = 'https://github.com'
+
+async function startCrawler() {
+  try {
+    const browser = await puppeteer.launch({
+      executablePath: '/home/manya/.nix-profile/bin/chromium'
+    })
+
+    const page = await browser.newPage()
+    await page.goto(URL)
+
+    const body = await page.locator('body')
+
+
+    console.log(body)
+
+    //await getFlatsLinksForOnePage(page)
+
+    await browser.close()
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function getFlatsLinksForOnePage(page) {
+  try {
+    console.log(page)
+    // const items = await page.locator('a[data-marker="item-title"]')
+    // console.log(items)
+    // const links = new Array(items).map(link => link.href)
+    // console.log(links)
+
+  } catch (error) {
+    console.error(error)
+  }  
+}
+
+startCrawler()
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.get('/', (request, response) => {
+  response.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'manya',
+  host: 'localhost',
+  database: 'avito',
+  password: 'password',
+  port: 5432,
+})
