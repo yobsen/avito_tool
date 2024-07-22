ef885fad439d5b335084f4fd566ca12b9a1cde94
Maria Elizarova <yobs.mary@gmail.com>
2024-07-23 03:20:19 +0400

    get flatsLinks for each page

--- crawler.js
+++ crawler.js
@@ -1,37 +1,28 @@
 const puppeteer = require('puppeteer-core')
 const { executablePath } = require('puppeteer')
 
-const URL = 'https://www.avito.ru/stavropol/kvartiry/prodam-ASgBAgICAUSSA8YQ?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA'
+const URL = 'https://www.avito.ru/stavropol/kvartiry/prodam-ASgBAgICAUSSA8YQ?context=H4sIAAAAAAAA_0q0MrSqLraysFJKK8rPDUhMT1WyLrYyNLNSKk5NLErOcMsvyg3PTElPLVGyrgUEAAD__xf8iH4tAAAA&s=104'
 
-async function startCrawler() {
+async function crawl() {
   try {
     const browser = await puppeteer.launch({
       headless: false,
       executablePath: executablePath(),
     })
     const page = await browser.newPage()
-    await page.goto(URL)
+    await page.goto(URL, { waitUntil: 'load', timeout: 0 })
 
-    await getFlatsLinksForOnePage(page)
-
-    await browser.close()
-
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function getFlatsLinksForOnePage(page) {
-  try {
-    console.log(page)
-      const items = await page.locator('a[data-marker="item-title"]')
-      console.log(items)
-      const links = new Array(items).map(link => link.href)
-      console.log(links)
+    const flatsLinksForOnePage = await page.$$eval('a[data-marker="item-title"]', items => { 
+      return items.map(link => link.href)
+    })
+    console.log(flatsLinksForOnePage)
 
   } catch (error) {
     console.error(error)
   }
+  //} finally {
+  //  browser?.close()
+  //}
 }
 
-startCrawler()
+crawl()
